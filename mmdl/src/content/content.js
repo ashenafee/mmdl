1863f52d6db3c754733c16ba4efd63d0b3234cc2
Ashenafee <ashenafeem@gmail.com>
2024-05-01 00:06:39 -0400

    Remove hardcoding for MP4 downloads and allow downloading in other formats

--- mmdl/src/content/content.js
+++ mmdl/src/content/content.js
@@ -1,5 +1,9 @@
-// Get the current URL
-var url = window.location.href;
+import mime from "mime";
+
+/**
+ * @type {string}
+ */
+const url = window.location.href;
 
 /**
  * Fetches the video chunks and returns a list of URLs.
@@ -8,24 +12,18 @@ var url = window.location.href;
  * @returns {Promise<string[]>} The list of URLs of the video chunks.
  */
 async function fetchVideoChunks(chunklistUrl) {
-    // Fetch
     const response = await fetch(chunklistUrl);
     const data = await response.text();
 
-    // Parse the chunklist data
     const chunklist = data.split("\n");
-
-    // Keep only the lines that contain the chunk URLs (.ts)
     const chunkLines = chunklist.filter((line) => line.endsWith(".ts"));
 
-    // Get the chunk URLs (https://stream.library.utoronto.ca:1935/MyMedia/play/mp4:1/<ID>.mp4/media_w548935148_0.ts)
     const chunks = chunkLines.map((line) => {
         const chunkUrl =
             chunklistUrl.substring(0, chunklistUrl.lastIndexOf("/") + 1) + line;
         return chunkUrl;
     });
 
-    // Return the list of URLs
     return chunks;
 }
 
@@ -54,41 +52,32 @@ async function loadChunk(chunkUrl) {
  */
 function downloadVideo(chunklistUrl, videoName, videoFormat) {
     fetchVideoChunks(chunklistUrl).then(async (chunks) => {
-        // Send a message to indicate that the download has started
-        chrome.runtime.sendMessage({
-            status: "downloadStarted",
-            totalChunks: chunks.length,
-        });
+        /** @type {chrome.runtime.MessageSender} */
+        const sendMessage = chrome.runtime.sendMessage;
 
-        // Load all chunks
+        sendMessage({ status: "downloadStarted", totalChunks: chunks.length });
+
         const chunkData = await Promise.all(
-            chunks.map((chunkUrl, index) => {
-                return loadChunk(chunkUrl).then((chunk) => {
-                    // Send a message for each chunk loaded
-                    chrome.runtime.sendMessage({
-                        status: "chunkLoaded",
-                        chunkIndex: index + 1,
-                    });
-                    return chunk;
-                });
+            chunks.map(async (chunkUrl, index) => {
+                const chunk = await loadChunk(chunkUrl);
+                sendMessage({ status: "chunkLoaded", chunkIndex: index + 1 });
+                return chunk;
             })
         );
 
-        // Concatenate all chunks into a single Blob
-        const videoBlob = new Blob(chunkData, { type: "video/mp4" });
-
-        // Create a download link for the video
+        const videoBlob = new Blob(chunkData, {
+            type: mime.getType(videoFormat),
+        });
         const videoUrl = URL.createObjectURL(videoBlob);
         const a = document.createElement("a");
         a.href = videoUrl;
-        a.download = videoName + "." + videoFormat;
+        a.download = `${videoName}.${videoFormat}`;
         a.style.display = "none";
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
 
-        // Send a message to indicate that the download has finished
-        chrome.runtime.sendMessage({ status: "downloadFinished" });
+        sendMessage({ status: "downloadFinished" });
     });
 }
 
@@ -98,16 +87,9 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             url.startsWith("https://play.library.utoronto.ca/") ||
             url.includes("play.library.utoronto.ca/")
         ) {
-            // Extract the video ID from the URL (last part of the URL)
-            var videoId = url.substring(url.lastIndexOf("/") + 1);
+            const videoId = url.substring(url.lastIndexOf("/") + 1);
+            const chunklistUrl = `https://stream.library.utoronto.ca:1935/MyMedia/play/mp4:1/${videoId}.mp4/chunklist.m3u8`;
 
-            // Construct the chunklist URL
-            var chunklistUrl =
-                "https://stream.library.utoronto.ca:1935/MyMedia/play/mp4:1/" +
-                videoId +
-                ".mp4/chunklist.m3u8";
-
-            // Fetch the video chunks
             downloadVideo(chunklistUrl, message.videoName, message.videoFormat);
         }
     }
